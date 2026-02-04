7fa2c56f97aef3d06667c2435fda8e6fd88bf802
BlackMugi <tapha10tapha97@gmail.com>
2026-02-04 13:22:24 +0000

    Initial commit

---
+++ Backend-App/couvoir-oeuficient/src/main/java/com/oeuficient/couvoir_oeuficient/CouvoirOeuficientApplication.java
@@ -0,0 +1,13 @@
+package com.oeuficient.couvoir_oeuficient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouvoirOeuficientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouvoirOeuficientApplication.class, args);
+	}
+
+}
---
+++ Backend-App/couvoir-oeuficient/src/test/java/com/oeuficient/couvoir_oeuficient/CouvoirOeuficientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oeuficient.couvoir_oeuficient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouvoirOeuficientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
